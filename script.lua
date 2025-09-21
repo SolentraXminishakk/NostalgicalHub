52b26ff9089c05b60c6f8967c872b14a1b4c8b8d
Solentra <soulxz2007@gmail.com>
2025-09-20 19:00:55 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,7 @@
-
+if tostring(remoteVersion) ~= LOCAL_VERSION then
+    warn("Nostalgic Hub | Update found! Auto-updating to " .. tostring(remoteVersion))
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SolentraXminishakk/NostalgicalHub/refs/heads/main/script.lua"))()
+    return
+else
+    print("Nostalgic Hub | Running latest version (" .. LOCAL_VERSION .. ")")
+end
